94838ab13b163eaec5de946c384dd37cbcbe0fcc
Pavel Mikhailovky <75169010+pavelmikhailovsky@users.noreply.github.com>
2023-02-06 05:18:46 +0300

    Delete entities/src/main/java/com/seabattle/entities/rules/build directory

--- entities/src/main/java/com/seabattle/entities/rules/build/ShipBuilding.java
+++
@@ -1,8 +0,0 @@
-package com.seabattle.entities.rules.build;
-
-import java.util.List;
-
-public interface ShipBuilding {
-
-    boolean canBuild(List<String> coordinates, List<DeckShip> deckShips, List<Coordinate> shipCoordinate);
-}
