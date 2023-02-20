0e8a79464581bf83ededecf9e121bc380a17be52
Pavel <dsvknsddfasfawe@gmail.com>
2023-02-20 20:26:54 +0300

    implementation of the HitShip interface

---
+++ entities/src/main/java/com/seabattle/entities/rules/hit/ShipDamage.java
@@ -0,0 +1,36 @@
+package com.seabattle.entities.rules.hit;
+
+import com.seabattle.entities.dto.Coordinate;
+import com.seabattle.entities.dto.DeckShip;
+
+import java.util.List;
+
+public class ShipDamage implements HitShip {
+
+    private DeckShip deckShip;
+    private final boolean isShipSank = true;
+    private List<Coordinate> damageCoordinates;
+    private List<Coordinate> coordinates;
+
+    public ShipDamage(DeckShip deckShip) {
+        this.deckShip = deckShip;
+    }
+
+    @Override
+    public DeckShip hit(Coordinate shipCoordinate) {
+        damageCoordinates = deckShip.getDamageCoordinates();
+        coordinates = deckShip.getCoordinates();
+
+        coordinates.remove(shipCoordinate);
+        deckShip.setCoordinates(coordinates);
+
+        damageCoordinates.add(shipCoordinate);
+        deckShip.setDamageCoordinates(damageCoordinates);
+
+        if (coordinates.isEmpty()) {
+            deckShip.setSank(isShipSank);
+        }
+
+        return deckShip;
+    }
+}
