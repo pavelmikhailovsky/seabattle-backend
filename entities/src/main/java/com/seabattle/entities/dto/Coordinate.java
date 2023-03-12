9685a104609c1382a5f892b5882c8321fd42b3de
Pavel <dsvknsddfasfawe@gmail.com>
2023-03-12 22:26:57 +0300

    deleted shipLink field

--- entities/src/main/java/com/seabattle/entities/dto/Coordinate.java
+++ entities/src/main/java/com/seabattle/entities/dto/Coordinate.java
@@ -5,24 +5,13 @@ import java.util.Objects;
 public class Coordinate {
 
     private final String content;
-    private final DeckShip shipLink;
 
     public Coordinate(String coordinate) {
         this.content = coordinate;
-        this.shipLink = null;
-    }
-
-    public Coordinate(String coordinate, DeckShip ship) {
-        this.content = coordinate;
-        this.shipLink = ship;
     }
 
     public String getContent() {
         return content;
-    }
-
-    public DeckShip getShipLink() {
-        return shipLink;
     }
 
     @Override
@@ -30,11 +19,11 @@ public class Coordinate {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Coordinate that = (Coordinate) o;
-        return Objects.equals(content, that.content) && Objects.equals(shipLink, that.shipLink);
+        return Objects.equals(content, that.content);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(content, shipLink);
+        return Objects.hash(content);
     }
 }
